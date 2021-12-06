64bf6f0358a8f6ab43161ec0b41381c9172c7647
ggaspar22 <andrijchornobajj@gmail.com>
2021-12-06 10:24:35 +0200

    added hw 11

---
+++ script.js
@@ -0,0 +1,31 @@
+// let a = prompt('Write your background');
+// let b = prompt('write type fonts');
+// let c = prompt('Leveling your h1');
+// let site_1 = prompt('Write your best site');
+// let site_2 = prompt('Write your best site');
+// let site_3 = prompt('Write your best site');
+let d = prompt('Write background for your link');
+
+
+// document.body.style.background = a;
+// document.body.style.fontStyle = b;
+// document.querySelector('.fhn').style.textAlign = c;
+
+// let s_1= document.createElement('a');
+// s_1.href = `https://${site_1}`+' ';
+// s_1.textContent = site_1;
+// console.log(s_1);
+// document.querySelector('.sites').after(s_1);
+
+// let s_2= document.createElement('a');
+// s_2.href = `https://${site_2}`+' ';
+// s_2.textContent = site_2;
+// document.querySelector('.sites').after(s_2);
+
+// let s_3= document.createElement('a');
+// s_3.href = `https://${site_3} `+' ';
+// s_3.textContent = site_3;
+// document.querySelector('.sites').after(s_3);
+
+document.querySelector('.sites').style.background = d;
+
